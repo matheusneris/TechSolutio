d1a5ceb077bd03e7a9f0c9be8b0890ecc61e5807
Matheus Neris <matheusneris7@gmail.com>
2023-04-14 22:06:39 -0300

    projeto criado

---
+++ src/test/java/com/cadastro/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
